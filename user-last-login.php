49b5a34b5f9ef12ed8f05bdab8f5cb8de2704259
Przemek Ciąćka <przemyslaw.ciacka@gmail.com>
2023-09-10 19:55:58 +0200

    Define plugin's header

--- user-last-login.php
+++ user-last-login.php
@@ -2,4 +2,10 @@
 
 /**
  * Plugin Name: User Last Login
+ * Description: Displays each user's most recent login time right in the WordPress user list, making user activity tracking a breeze.
+ * Version: 1.0.0
+ * Author: Przemek Ciąćka
+ * Author URI: https://github.com/pciacka
+ *
+ * Text Domain: user-last-login
  */
